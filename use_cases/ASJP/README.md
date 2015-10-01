45098721e384fe9f8485d83a3f47c358afc8642e
Michael Cysouw <cysouw@mac.com>
2015-10-01 10:45:17 +0200

    usability update to ASJP use case

--- use_cases/ASJP/README.md
+++ use_cases/ASJP/README.md
@@ -2,9 +2,12 @@
 
 ###Michael Cysouw
 <cysouw@mac.com>
+
 30 September 2015
 
-The ASJP-data are available online at http://asjp.clld.org (Wichmann, Brown & Holman, 2014). The original downloaded data is available verbatim in "/sources/".
+###Data preparation
+
+The ASJP-data are available online at <http://asjp.clld.org/static/download/asjp-dataset.cldf.zip> (Wichmann, Brown & Holman, 2014). The original downloaded data is available verbatim in the directory `sources`.
 
 The downloaded data comes in separate files, so I combined everything into one large csv file by using the following bash command. I then manually added a new header line.
 
@@ -14,7 +17,11 @@ When you have csvkit installed, it can be done cleaner by:
 
 `$$$ csvstack sources/*.csv > data/ASJP.csv`
 
+###Orthographic processing
+
+The steps to process the orthography of the ASJP words using R is found in `manual.R`. The file `manual.html` shows what happens when you run all these commands (produced by `rmarkdown::render`). Any files written by these commands are by default saved into the directory `sandbox`. The intended results can also be found in the directory `data`.
+
 ###References
 
-Wichmann, Søren & Brown, Cecil H. & Holman, Eric W. 2014. The The ASJP Database Database. Leipzig: Max Planck Institute for Evolutionary Anthropology. (Available online at http://asjp.clld.org, Accessed on 2015-08-31.)
+Wichmann, Søren & Brown, Cecil H. & Holman, Eric W. 2014. The The ASJP Database Database. Leipzig: Max Planck Institute for Evolutionary Anthropology. (Available online at <http://asjp.clld.org>, Accessed on 2015-08-31.)
 
--- use_cases/ASJP/manual.R
+++ use_cases/ASJP/manual.R
@@ -4,8 +4,8 @@
 #' date: "30 September 2015"
 #' ---
 
+#' === STEP 0: Preparation ===
 # install library from github
-# only necessary once
 # install.packages("devtools")
 # devtools::install_github("cysouw/qlcTokenize")
 library(qlcTokenize)
@@ -14,41 +14,47 @@ library(qlcTokenize)
 # this is pretty large, so it might take a bit
 asjp <- read.csv("data/ASJP.csv")
 
+#' === STEP 1: making a draft profile ===
 # the actual words are in the fifth column of this csv-file ("Value")
 # first make draft profile using default settings
-write.profile(asjp$Value, file = "sandbox/asjp_draft_profile.csv")
+write.profile(asjp$Value, file = "sandbox/1_asjp_draft_profile.tsv")
 
+#' === STEP 2: correcting the draft profile ===
 # There is a warning because there are tabs in the input data
 # This is an error in the ASJP data, but we leave it in for now
 # It has to be changed in the orthography profile though!
-# Check the manually changed profile in profiles/asjp_corrected_profile.csv
+# Check the manually changed profile in data/2_asjp_corrected_profile.tsv
+# This profile should be capturing the ASJP orthography
 
-
+#' === STEP 3: tokenize the data and inspect errors ===
 # after manually correcting the profile we can use it for tokenization
 # there are regular expressions in the corrected profile, so "regex = TRUE"
+# save all the information from the tokenization in the sandbox
 # the code is not very fast, might take about half a minute for all of ASJP
 tokens <- tokenize(asjp$Value
-					, profile = "profiles/asjp_corrected_profile.csv"
+					, profile = "data/2_asjp_corrected_profile.tsv"
 					, regex = TRUE
+					, file.out = "sandbox/3_asjp_result"
 					)
-					
 # there are errors in the data. Let's check the errors
 tokens$errors
-
 # and look at the characters that are not in the profile
 tokens$missing
 
-# look at the errors in this original context
-error_lines <- as.numeric(rownames(tokens$errors))
+# look at the errors in their original context
+error_lines <- rownames(tokens$errors)
 asjp[error_lines,]
 
+#' === STEP 4: error correction and investigate tailored graphemes ===
 # remove errors for now (this has to be done in the original data of course!)
-# make "empirical" profile of graphemes, showing all tailored graphemes in ASJP
-# there are 793 different tailored graphemes in the data !!!
-no_errors <- tokens$strings$tokenized[-error_lines]
-tailored <- write.profile(no_errors, sep = " ")
-str(tailored)
-head(tailored, 20)
+no_errors <- tokens$strings[-as.numeric(error_lines),]
+# make "empirical" profile of tokenized data, showing all tailored graphemes in ASJP
+tailored <- write.profile(no_errors$tokenized
+						, sep = " "
+						, file.out = "sandbox/4_asjp_tailored_profile.tsv"
+						)
 
-# save the profile as a file
-write.profile(no_errors, sep = " ", file = "sandbox/asjp_emprical_profile.csv")
+# there are 793 different tailored graphemes in the data !!!
+str(tailored)
+# just show the first 20 tailored graphemes as an example
+head(tailored, 20)
