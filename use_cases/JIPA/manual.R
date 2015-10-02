fa291d781d61a4384a57b0fe6a540e9f6faf08d4
Michael Cysouw <cysouw@mac.com>
2015-10-02 21:00:06 +0200

    update JIPA manual.R

--- use_cases/JIPA/manual.R
+++ use_cases/JIPA/manual.R
@@ -1,13 +1,16 @@
 #' ---
 #' title: "Tokenization of JIPA transcriptions"
 #' author: "Michael Cysouw"
-#' date: "2 October 2015"
+#' date: "`r Sys.Date()`"
 #' ---
 
 library(qlcTokenize)
 
+#' ### Check automatic tokenization
+# check it against hand-made tokenization
 # adhoc function to compare tokenization with hand-prepared results
-compare <- function(jipa) {
+
+compare <- function(jipa, profile) {
 	text_in <- paste0("data/", jipa, "_input.txt")
 	text_out <- paste0("data/", jipa, "_output.txt")
 	
@@ -15,19 +18,63 @@ compare <- function(jipa) {
 	output <- scan(text_out, what = "character", sep = "\n")
 	
 	tokenized <- tokenize(input
-						, profile = "profiles/validIPA2005_graphemes.tsv"
+						, profile = profile
 						, sep = " "
 						, sep.replace = "#"
-						, file = "tmp"
 						, regex = TRUE
 						, normalize = "NFD"
-						)
+						)						
+
 	all.equal(output, tokenized$strings$tokenized)
 
 }
 
-# all true!
-compare("Brazilian_Portuguese")
-compare("Kabiye")
-compare("Vietnamese")
-compare("Zurich_German")
+# all true as valid IPA!
+profile <- "profiles/validIPA2005_graphemes.tsv"
+compare("Brazilian_Portuguese", profile)
+compare("Kabiye", profile)
+compare("Zurich_German", profile)
+compare("Vietnamese", profile)
+
+# not all conform to the strict IPA interpretation
+profile <- "profiles/strictIPA2005_graphemes.tsv"
+compare("Brazilian_Portuguese", profile)
+compare("Kabiye", profile)
+compare("Zurich_German", profile)
+compare("Vietnamese", profile)
+
+#' ### Tailored grapheme clusters
+# list all different tailored graphemes with frequencies
+# write the profiles with the tailored graphemes to the sandbox
+# disaply requires the knitr library
+require("knitr")	
+
+tailored <- function(jipa) {
+	input <- scan(paste0("data/", jipa, "_input.txt")
+				, what = "character"
+				, sep = "\n"
+				)
+	
+	tokenized <- tokenize(input
+						, profile = "profiles/validIPA2005_graphemes.tsv"
+						, sep = " "
+						, sep.replace = ""
+						, regex = TRUE
+						, normalize = "NFD"
+						)
+
+	profile <- write.profile(tokenized$strings$tokenized
+				, sep = " "
+				, file.out = paste0("sandbox/", jipa, "_profile.tsv")
+				)
+				
+		
+	invisible(knitr::kable(profile))
+}
+
+# write all to sandbox
+tailored("Brazilian_Portuguese")
+tailored("Kabiye")
+tailored("Zurich_German")
+# only show Vietnamese here in a nice format
+(tailored("Vietnamese"))
