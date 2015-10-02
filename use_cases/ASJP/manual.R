1da834453f5cf4d9f02b3e90c55a44a9f4c0c3a8
Michael Cysouw <cysouw@mac.com>
2015-10-02 20:59:09 +0200

    update ASJP manual.R

--- use_cases/ASJP/manual.R
+++ use_cases/ASJP/manual.R
@@ -1,32 +1,34 @@
 #' ---
 #' title: "Analysis of ASJP data"
 #' author: "Michael Cysouw"
-#' date: "30 September 2015"
+#' date: "`r Sys.Date()`"
 #' ---
 
-#' === STEP 0: Preparation ===
+#' ### STEP 0: Preparation
 # install library from github
 # install.packages("devtools")
 # devtools::install_github("cysouw/qlcTokenize")
 library(qlcTokenize)
+# for the nice display of tables
+require(knitr)
 
 # read ASJP data
 # this is pretty large, so it might take a bit
 asjp <- read.csv("data/ASJP.csv")
 
-#' === STEP 1: making a draft profile ===
+#' ### STEP 1: making a draft profile
 # the actual words are in the fifth column of this csv-file ("Value")
 # first make draft profile using default settings
 write.profile(asjp$Value, file = "sandbox/1_asjp_draft_profile.tsv")
 
-#' === STEP 2: correcting the draft profile ===
+#' ### STEP 2: correcting the draft profile
 # There is a warning because there are tabs in the input data
 # This is an error in the ASJP data, but we leave it in for now
 # It has to be changed in the orthography profile though!
 # Check the manually changed profile in data/2_asjp_corrected_profile.tsv
 # This profile should be capturing the ASJP orthography
 
-#' === STEP 3: tokenize the data and inspect errors ===
+#' ### STEP 3: tokenize the data and inspect errors
 # after manually correcting the profile we can use it for tokenization
 # there are regular expressions in the corrected profile, so "regex = TRUE"
 # save all the information from the tokenization in the sandbox
@@ -37,15 +39,15 @@ tokens <- tokenize(asjp$Value
 					, file.out = "sandbox/3_asjp_result"
 					)
 # there are errors in the data. Let's check the errors
-tokens$errors
+knitr::kable(tokens$errors)
 # and look at the characters that are not in the profile
-tokens$missing
+knitr::kable(tokens$missing)
 
 # look at the errors in their original context
 error_lines <- rownames(tokens$errors)
-asjp[error_lines,]
+knitr::kable(asjp[error_lines,])
 
-#' === STEP 4: error correction and investigate tailored graphemes ===
+#' ### STEP 4: error correction and investigate tailored graphemes
 # remove errors for now (this has to be done in the original data of course!)
 no_errors <- tokens$strings[-as.numeric(error_lines),]
 # make "empirical" profile of tokenized data, showing all tailored graphemes in ASJP
@@ -57,4 +59,4 @@ tailored <- write.profile(no_errors$tokenized
 # there are 793 different tailored graphemes in the data !!!
 str(tailored)
 # just show the first 20 tailored graphemes as an example
-head(tailored, 20)
+knitr::kable(head(tailored, 20))
